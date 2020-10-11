682e12a562a574c4f647fda29c1c42ec91ca7f48
Kevin Valias <Kevbodyworks@gmail.com>
2020-10-10 21:27:28 -0500

    What is Javascript

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,9 @@
+
+console.log('hello world')
+//  What in the world is Javascript?? //
+// Its a lightweight language(doesnt eat up much memory on the computer), cross platform(can be used on multiple platforms and systems not just web development), object oriented (language basedd on objects) computer programming language.
+
+// HTML = content /Nouns
+// CSS = Representation /Adjective
+// JS = Dynamic Effects/ Programming /Verb
+

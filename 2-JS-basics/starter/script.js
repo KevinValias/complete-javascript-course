de1fb06c34ce1bc6a1bdce48738a5e4df33f824c
Kevin Valias <Kevbodyworks@gmail.com>
2020-10-10 21:29:10 -0500

    Variables and Data Types

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -7,3 +7,29 @@ console.log('hello world')
 // CSS = Representation /Adjective
 // JS = Dynamic Effects/ Programming /Verb
 
+/* Variables & Data Types */
+
+// const firstName = 'John';
+// console.log('firstName');
+
+// Variable = firstName Value= John
+//  A VARIABLE in Javascript is like a container in which we can store a value in order to use it again and again in our code. Variables are Camel-Cased.
+
+// const lastName = 'Smith';
+// const age = 28;
+
+
+// In Javascript there are 5 different kind of Data Types
+// Number = Floating point numbers, for decimals and integers. //
+// String = Sequence of characters, used for text //
+// Boolean = Logical data type that can only be true or false //
+// const fullAge = 'true'
+// console.log(fullAge);
+// Undefined = Data type of variable that does not have a value yet. // 
+// var job;
+// console.log(job);
+// Null = Also means 'non existent' //
+
+// job = 'Teacher';
+// console.log(job);
+
